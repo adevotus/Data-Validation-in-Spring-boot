17ca76f2e0b1e2f447646b9a80428ae46d071949
adevotus <adevoweb@gmail.com>
2023-03-14 12:50:27 +0300

    Expection handler

--- src/main/java/com/example/validation/advice/ExceptionHandlerApp.java
+++ src/main/java/com/example/validation/advice/ExceptionHandlerApp.java
@@ -1,16 +1,20 @@
 package com.example.validation.advice;
 
 
+import jakarta.validation.UnexpectedTypeException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 @RestControllerAdvice
 public class ExceptionHandlerApp {
-
+/********************************
+ The expection handler for the customer messages
+ * ******************************************/
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleExption(MethodArgumentNotValidException exception) {
         Map<String, String> errorMap = new HashMap<>();
@@ -19,5 +23,14 @@ public class ExceptionHandlerApp {
         });
 
         return errorMap;
+    }
+
+    @ExceptionHandler(UnexpectedTypeException.class)
+    public Map<String , String> handleExption(UnexpectedTypeException exception){
+        Map<String, String> errorMap = new HashMap<>();
+        exception.getLocalizedMessage();
+        Logger.getGlobal().info("this is an error from size");
+        return errorMap;
+
     }
 }
--- src/main/java/com/example/validation/dto/UserRequestDTO.java
+++ src/main/java/com/example/validation/dto/UserRequestDTO.java
@@ -11,10 +11,13 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserRequestDTO {
 
-//    @NotNull(message = "the name should not be null")
+
+    /*********************************
+     Validation for the input field that is required for the users
+     * ****************************************/
     @NotBlank(message = "Name is mandatory")
     @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must only contain alphabetic characters")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must only contain alphabetic characters with no space")
     private String name;
 
     @NotNull(message = "the address should not be null")
@@ -24,9 +27,10 @@ public class UserRequestDTO {
     private String email;
 
 
-    @Size(min = 10, max = 50, message = "your above the required age, btn 10 and 50")
+    @NotNull(message = "Age is mandatory")
+    @Min(value = 18, message = "Age must be at least 18")
+    @Max(value = 150, message = "Age must be at most 150")
     private Integer age;
 
-    //    @NotEmpty
     private Double salary;
 }
